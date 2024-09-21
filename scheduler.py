5c24b1f7d5929ddabbd62c9828cda13c5388fe94
Hiroto Tamura <h.tamura@astroscale.com>
2024-09-21 21:55:29 +0900

    Add scheduler

---
+++ scheduler.py
@@ -0,0 +1,26 @@
+import sys
+import schedule
+import time
+import webbrowser
+
+def open_browser():
+    webbrowser.open("http://localhost:5000")
+
+
+def schedule_browser_opening():
+    # Schedule the open_browser function based on user input
+    schedule.every().day.at(sys.argv[1]).do(open_browser)
+    print(f"Browser will open at {sys.argv[1]}.")
+
+
+def main():
+
+    schedule_browser_opening()
+    # Run the scheduler loop
+    while True:
+
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
